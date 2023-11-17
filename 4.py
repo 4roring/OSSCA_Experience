62d71a24f4d7afa6f2dcf810d0c3f96726666ea4
4roring <4roring@naver.com>
2023-11-18 00:49:18 +0900

    4번 완료

--- 4.py
+++ 4.py
@@ -1,4 +1,10 @@
+import pickle
 import pandas as pd
+import matplotlib.pylab as plt
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
 
 if __name__ == "__main__":
     df = pd.DataFrame(
@@ -7,3 +13,25 @@ if __name__ == "__main__":
             "score": [60, 63, 64, 67, 68, 71, 72, 75, 76, 78],
         }
     )
+    df_x = df["hours"]
+    df_y = df["score"]
+
+    plt.scatter(df_x, df_y, label="data")
+    plt.xlabel("hours")
+    plt.ylabel("score")
+    plt.show()
+
+    x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.3)
+
+    lr = LinearRegression()
+    lr.fit(x_train.values.reshape(-1, 1), y_train)
+    y_pred = lr.predict(x_test.values.reshape(-1, 1))
+    print("Mean Squared Error:", metrics.mean_squared_error(y_test, y_pred))
+
+    save_lr = pickle.dumps(lr)
+    load_lr = pickle.loads(save_lr)
+
+    hour_12 = load_lr.predict([[12]])
+    hour_14 = load_lr.predict([[14]])
+    print(hour_12)
+    print(hour_14)
